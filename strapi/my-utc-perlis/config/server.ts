ba0d3a91f76eb7ebce98fa2d201f34ae56f45043
saufisyafiq <saufimns@gmail.com>
2025-08-25 06:42:43 +0800

    Token transfer salt setting

--- strapi/my-utc-perlis/config/server.ts
+++ strapi/my-utc-perlis/config/server.ts
@@ -1,4 +1,12 @@
 export default ({ env }) => ({
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+    remote: {
+      enabled: true, // Enable data transfer
+    },
+  },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
